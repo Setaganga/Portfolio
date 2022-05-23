6f49830e2dcd354764528dc7cd4c922e8b157885
Joren <94319063+Setaganga@users.noreply.github.com>
2022-05-22 20:40:24 -0600

    Update howmanydaysagobirthday.py

--- ArrangedGithubPortfolio/Random Projects/howmanydaysagobirthday.py
+++ ArrangedGithubPortfolio/Random Projects/howmanydaysagobirthday.py
@@ -12,7 +12,7 @@ def birthdaycalc(birthday,withTime = False):
             hour = hour[0]
         minute = str(TD)[14:16]
         if minute[-1] == ":":
-            minute = hour[0]
+            minute = minute[0]
         result = f"Years:{TD.days // 365} Days:{TD.days % 365} Hours:{hour} Minutes:{minute}"
         return result
     else:
@@ -23,6 +23,6 @@ def birthdaycalc(birthday,withTime = False):
         return result
 
 print(birthdaycalc("2007-4-7",False))
-print(birthdaycalc("1974-5-4 19:07",True))
+print(birthdaycalc("2007-4-7 19:07",True))
 print(birthdaycalc("1974-5-4",False))
 print(birthdaycalc("1974-3-15",False))

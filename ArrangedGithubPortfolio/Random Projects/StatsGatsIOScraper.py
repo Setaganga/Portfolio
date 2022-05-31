f90633ce06ed4568f3430db69c3f8f9d5c686aa9
Joren <94319063+Setaganga@users.noreply.github.com>
2022-05-31 09:44:59 -0600

    Update StatsGatsIOScraper.py

--- ArrangedGithubPortfolio/Random Projects/StatsGatsIOScraper.py
+++ ArrangedGithubPortfolio/Random Projects/StatsGatsIOScraper.py
@@ -5,7 +5,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-#Getting user input on how many clans and what data they want
+#Getting user input on how many clans and which data they want
 count = 0
 datatype = 0
 while count <= 0 or count >= 25:
@@ -17,19 +17,17 @@ while datatype <= 0 or datatype >= 6:
 page = requests.get("https://stats.gats.io/clans/top")
 sc = page.status_code
 print(f"Status code: {sc}")
-
+print("\n")
+#Check if its a-okay
 if sc != 200:
     print("ERROR: Website Content could not be retrieved") #Oopsies! Data could not be retrieved!
 else:
     bs = BeautifulSoup(page.content, 'html.parser')#Get entire html page
     table = bs.find(class_="rowlink")#Get specific table
-    somedata = table.findAll("tr")#Get all table rows
+    rows = table.findAll("tr")#Get all table rows
 
     for x in range(count): #For count number of times
-        data = list(somedata[x].children) #Data is a list of the children of tr (this includes stuff like <td>1</td> and "\n")
-        for y in data:
-            if y == "\n":
-                data.remove(y) #Loop through the data and get rid of newlines
-        print(data[datatype - 1].get_text().strip()) #Oh lord this is tough to explain. Data now looks something like "<td>1</td>,<td>clantag</td>,<td>clanname</td>"
-        #So the reason I do minus one is because I prompted the user 1-5 and list indices start at 0.
-        #So lets say I picked 3 at the start for the clan name. It gives me data[2], then removes the tags, and strips whitespace.
+        data = list(rows[x].children) #Data is a list of the children of tr (this includes stuff like <td>1</td> and "\n")
+        data = [y for y in data if y != "\n"]
+        print(data[datatype - 1].get_text().strip()) #Get text and strip of whitespace from data and index of var datatype - 1 (list indices start at 0)
+print("\n")

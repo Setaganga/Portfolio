dd756764733a88d38f66c1bee286654aa2f5de09
Joren <94319063+Setaganga@users.noreply.github.com>
2022-05-31 13:53:40 -0600

    Update StatsGatsIOScraper.py

--- ArrangedGithubPortfolio/Random Projects/StatsGatsIOScraper.py
+++ ArrangedGithubPortfolio/Random Projects/StatsGatsIOScraper.py
@@ -6,11 +6,18 @@ import requests
 from bs4 import BeautifulSoup
 
 #Getting user input on how many clans and which data they want
-count, datatype = 0
-while count <= 0 or count >= 25:
-    count = int(input("Enter amount of clans to scrape (1-25):"))
+Tmin = 0
+Tmax = 0
+datatype = 0
+while Tmin <= 0 or Tmin > 25:
+    Tmin = int(input("Enter starting num to scrape (1-25):"))
+while Tmax <= 0 or Tmax > 25:
+    Tmax = int(input("Enter ending num to scrape (1-25):"))
 while datatype <= 0 or datatype >= 6:
     datatype = int(input("Enter 1-5 if you would like ID, Tag, Name, Players, or Score:"))
+if not (Tmax >= Tmin):
+    print("ERROR: Max is not greater than or equal to min.\nRetry and make sure min is <= max")
+    exit()
 
 #Send request to server to get data
 page = requests.get("https://stats.gats.io/clans/top")
@@ -25,7 +32,7 @@ else:
     table = bs.find(class_="rowlink")#Get specific table
     rows = table.findAll("tr")#Get all table rows
 
-    for x in range(count): #For count number of times
+    for x in range(Tmin - 1, Tmax): #For count number of times
         data = list(rows[x].children) #Data is a list of the children of tr (this includes stuff like <td>1</td> and "\n")
         data = [y for y in data if y != "\n"]
         print(data[datatype - 1].get_text().strip()) #Get text and strip of whitespace from data and index of var datatype - 1 (list indices start at 0)

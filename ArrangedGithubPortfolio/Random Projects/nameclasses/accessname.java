a3beb37da753aa06dfa58ebfc81980ea25fc709c
Joren <94319063+Setaganga@users.noreply.github.com>
2022-06-06 10:19:35 -0600

    Add files via upload

---
+++ ArrangedGithubPortfolio/Random Projects/nameclasses/accessname.java
@@ -0,0 +1,9 @@
+package nameclasses;
+
+public class accessname {
+    public static void main(String[] args) {
+        name personJose = new name();
+        personJose.SetName("Jose");
+        System.out.println(personJose.GetName());
+    }
+}
---
+++ ArrangedGithubPortfolio/Random Projects/nameclasses/name.java
@@ -0,0 +1,11 @@
+package nameclasses;
+
+public class name {
+    private String fName;
+    public String GetName() {
+        return(this.fName);
+    }
+    public void SetName(String argName) {
+        this.fName = argName;
+    }
+}

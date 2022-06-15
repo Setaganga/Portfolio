38783a6193b716c58e6384e0f146510e16a595da
Joren <94319063+Setaganga@users.noreply.github.com>
2022-06-14 18:37:44 -0600

    Update designerdoormat.py

--- ArrangedGithubPortfolio/Challenges/designerdoormat.py
+++ ArrangedGithubPortfolio/Challenges/designerdoormat.py
@@ -6,6 +6,12 @@ welcomePos = (n // 2) + 1
 changNum = 3
 dotpipedot = 1
 md2 = m // 2
+if n % 2 != 1:
+    print("N must be odd to create!")
+    exit()
+if m != (n * 3):
+    print(f"M must be N times 3!\nTo help out your pea brain, it's {n * 3}.")
+    exit()
 for _ in range(welcomePos - 1):
     print(f'{"-"* (md2 - (changNum - 2)) }{".|." * dotpipedot}{"-"* (md2 - (changNum - 2))}')
     changNum += 3

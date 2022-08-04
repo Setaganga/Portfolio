4d59ffcf419779fc229ca4279bb667fb911ade56
Joren <94319063+Setaganga@users.noreply.github.com>
2022-08-03 19:58:58 -0600

    Update Rsummonsignupdater.py

--- Portfolio/Rsummonsignupdater.py
+++ Portfolio/Rsummonsignupdater.py
@@ -14,6 +14,7 @@ from bs4 import BeautifulSoup
 from time import sleep
 import re
 from termcolor import colored
+
 websiteLink = "https://new.reddit.com/r/SummonSign/?f=flair_name%3A%22%3Apsx%3A%20Help%20Me!%22"
 
 passablePlatforms = ["PS4", "PS5", "PS4/5"]
@@ -74,46 +75,46 @@ while True:
     sc = req.status_code
     if sc != 200:
         print(colored("Could not fetch website","red"))
-        break
-    #Use html parser
-    bs = BeautifulSoup(req.content,'html.parser')
-    #Find the div with the posts
-    helpmesections = bs.find(class_="rpBJOHq2PR60pnwJlUyP0")
-    #Get timestamps
-    timestamp = helpmesections.findAll("span")
-    #Get post title
-    title = helpmesections.findAll(class_="_eYtD2XCVieq6emjKBH3m")
-    #Get comment count
-    comments = helpmesections.findAll(class_="FHCV02u6Cp2zYL0fhQPsO")
-    #Make sure timestamp has "ago" inside of it
-    refinedtimestamp = [x.get_text() for x in timestamp if "ago" in x.get_text()]
-    #Get text of title
-    refinedtitle = [x.get_text() for x in title]
-    #Get text from comments
-    refinedcomments = [x.get_text() for x in comments]
+    else:
+        #Use html parser
+        bs = BeautifulSoup(req.content,'html.parser')
+        #Find the div with the posts
+        helpmesections = bs.find(class_="rpBJOHq2PR60pnwJlUyP0")
+        #Get timestamps
+        timestamp = helpmesections.findAll("span")
+        #Get post title
+        title = helpmesections.findAll(class_="_eYtD2XCVieq6emjKBH3m")
+        #Get comment count
+        comments = helpmesections.findAll(class_="FHCV02u6Cp2zYL0fhQPsO")
+        #Make sure timestamp has "ago" inside of it
+        refinedtimestamp = [x.get_text() for x in timestamp if "ago" in x.get_text()]
+        #Get text of title
+        refinedtitle = [x.get_text() for x in title]
+        #Get text from comments
+        refinedcomments = [x.get_text() for x in comments]
 
-    #Check if lists arent empty
-    if len(refinedtitle) > 0:
-        #If it has 5 or more, print out the first 5
-        if len(refinedtitle) > 4:
-            for i in range(5):
-                if PS_DS3_Validate(refinedtitle[i]):
-                    if refinedcomments[i] == "0 comments":
-                        print(colored("*CRITERIA* ","green"),colored("*NOT TAKEN*","blue"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
+        #Check if lists arent empty
+        if len(refinedtitle) > 0:
+            #If it has 5 or more, print out the first 5
+            if len(refinedtitle) > 4:
+                for i in range(5):
+                    if PS_DS3_Validate(refinedtitle[i]):
+                        if refinedcomments[i] == "0 comments":
+                            print(colored("*CRITERIA* ","green"),colored("*NOT TAKEN*","blue"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
+                        else:
+                            print(colored("*CRITERIA*","green"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
                     else:
-                        print(colored("*CRITERIA*","green"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
-                else:
-                    print(f"{refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
-        #otherwise, print out available posts.
-        else:
-            for i in range(len(refinedtitle)):
-                if PS_DS3_Validate(refinedtitle[i]):
-                    if refinedcomments[i] == "0 comments":
-                        print(colored("*CRITERIA* ","green"),colored("*NOT TAKEN*","blue"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
+                        print(f"{refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
+            #otherwise, print out available posts.
+            else:
+                for i in range(len(refinedtitle)):
+                    if PS_DS3_Validate(refinedtitle[i]):
+                        if refinedcomments[i] == "0 comments":
+                            print(colored("*CRITERIA* ","green"),colored("*NOT TAKEN*","blue"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
+                        else:
+                            print(colored("*CRITERIA*","green"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
                     else:
-                        print(colored("*CRITERIA*","green"),f" {refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
-                else:
-                    print(f"{refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
+                        print(f"{refinedtitle[i]}, {refinedtimestamp[i]}, {refinedcomments[i]}\n")
 
-    #wait 5 minutes
-    sleep(300)
+    #wait 3 minutes
+    sleep(180)
